04cc24cc742d03dbb40e43d01ac83ad651cd4be5
Lucas Martinez LLosco <linkgz55@gmail.com>
2022-10-25 23:52:34 -0300

    Se pusieron los botones para ir al nivel 1 y 2 del menu -Lucas Martinez

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,25 +1,28 @@
 import Phaser from "phaser";
-import { Button } from '../components/Button.js';
-
+import { Button1 } from "../components/Button1"; //para el nivel 1
+import { Button2 } from "../components/Button2"; //para el nivel 2
 import menuMusic from '../assets/sounds/menuMusic.mp3';
 export default class Menu extends Phaser.Scene{
     musicMenu = null;
     constructor(){
        super({key : 'Menu'}); //Key para su llamado
-       this.restartButton = new Button(this); //se reutiliza el boton utilizado en "gameOver" para que cumpla su cometido de ir a principal 
+       this.restartButton = new Button1(this); //se crea un obj de la clase Button que permitira ir a nivel 1, se pasa de parametro esta escena
+       this.restartButton2 = new Button2(this); //lo mismo pero para el nivel 2
     }
 
     preload(){
         this.load.audio('menuMusic', menuMusic);
         this.load.image('Menu','img/menu.png'); //se carga la imagen
-        this.restartButton.preload(); // se llama a la funcion preload que cargara la imagen del button
+        this.restartButton.preload(); // se llama a la funcion preload que cargara la imagen del button1
+        this.restartButton2.preload(); // se llama a la funcion preload que cargara la imagen del button2
     }
 
     create(){
         this.musicMenu = this.sound.add('menuMusic');
         this.musicMenu.play({loop: true, volume: 0.5});
         this.add.image(450,200,'Menu').setScale(.3); //se agrega a la imagen y se modifica el tamaño
-        this.restartButton.create();//Aqui se llama la funcion para mostrar el button en la escena
+        this.restartButton.create();//Aqui se llama la funcion para mostrar el button1 en la escena
+        this.restartButton2.create();//Aqui se llama la funcion para mostrar el button2 en la escena
     }
 }
 
